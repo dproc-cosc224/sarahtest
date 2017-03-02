41792ca7b0f8008d80cfec79e7a22595121e5014
Martin Wallace <martin.v.wallace@ieee.org>
2017-03-01 18:08:27 -0800

    Add basic ghost AI for pacman with BFS and DFS

--- src/Game.js
+++ src/Game.js
@@ -8,15 +8,26 @@
 
 Game.Game = function(game) { };
 
+
 var TILE_SIZE = 32;
+var WIDTH = 20;
+var HEIGHT = 28;
+
+// keep a list of terrains which are impassable
 var IMPASSABLE_TERRAIN = [0];
-var startX = 1 * TILE_SIZE + 16;
-var startY = 18 * TILE_SIZE + 16;
+
+// start in bottom right corner
+var startX = TILE_SIZE + 16;
+var startY = 26 * TILE_SIZE + 16;
+
+var ghostX = 9 * TILE_SIZE + 16;
+var ghostY = 11 * TILE_SIZE + 16;
 
 var map;
 var layer;
 var tiles;
 var pac;
+var ghost;
 var cursors;
 
 var speed = 150;
@@ -28,10 +39,10 @@ var opposites = [Phaser.None, Phaser.RIGHT, Phaser.LEFT, Phaser.DOWN, Phaser.UP]
 
 // use these for sprite rotations -- assumes that sprite starts facing to the right
 var targetAngleTable = {};
-targetAngleTable[Phaser.DOWN] = 90;
-targetAngleTable[Phaser.UP] = 270;
 targetAngleTable[Phaser.RIGHT] = 0;
+targetAngleTable[Phaser.DOWN] = 90;
 targetAngleTable[Phaser.LEFT] = 180;
+targetAngleTable[Phaser.UP] = 270;
 
 var current = Phaser.RIGHT;
 var turning = Phaser.NONE;
@@ -57,6 +68,10 @@ Game.Game.prototype = {
         pac = game.add.sprite(startX, startY, 'pacman', 0);
         pac.anchor.setTo(0.5, 0.5);
         this.physics.arcade.enable(pac);
+
+        ghost = game.add.sprite(ghostX, ghostY, 'ghost', 0);
+        ghost.anchor.setTo(0.5, 0.5);
+        this.physics.arcade.enable(ghost);
         cursors = game.input.keyboard.createCursorKeys();
 
         this.move(Phaser.RIGHT);
@@ -76,16 +91,19 @@ Game.Game.prototype = {
 
         var index = layer.index;
 
-        directions[1] = map.getTileLeft(index, x, y);
-        directions[2] = map.getTileRight(index, x, y);
-        directions[3] = map.getTileAbove(index, x, y);
-        directions[4] = map.getTileBelow(index, x, y);
+        // LEFT, RIGHT, UP, and DOWN correspond to 1, 2, 3, and 4
+        directions[Phaser.LEFT] = map.getTileLeft(index, x, y);
+        directions[Phaser.RIGHT] = map.getTileRight(index, x, y);
+        directions[Phaser.UP] = map.getTileAbove(index, x, y);
+        directions[Phaser.DOWN] = map.getTileBelow(index, x, y);
 
         this.checkKeys();
 
         if(turning != Phaser.NONE) {
             this.turn();
         }
+
+        this.ghost_1(true);
     },
 
     checkKeys: function() {
@@ -180,6 +198,115 @@ Game.Game.prototype = {
             return targetAngleNeg;
         }
         return targetAngle;
-    }
+    },
 
+    ghost_1: function(dfs) {
+
+        ghost.body.velocity.x = 0;
+        ghost.body.velocity.y = 0;
+
+        var g_x = this.math.snapToFloor(Math.floor(ghost.x), TILE_SIZE) / TILE_SIZE;
+        var g_y = this.math.snapToFloor(Math.floor(ghost.y), TILE_SIZE) / TILE_SIZE;
+
+        var p_x = this.math.snapToFloor(Math.floor(pac.x), TILE_SIZE) / TILE_SIZE;
+        var p_y = this.math.snapToFloor(Math.floor(pac.y), TILE_SIZE) / TILE_SIZE;
+
+        var index = layer.index;
+
+        var seen = new Array(WIDTH);
+        for(var i = 0; i < WIDTH; i++) {
+            seen[i] = new Array(HEIGHT).fill(false);
+        }
+
+        var q = [];
+        q.push([p_x, p_y]);
+
+        while(q.length != 0) {
+
+            // pop will give you a DFS, shift will give you a BFS e.g. Stack vs Queue
+            var cur = 0;
+            if(dfs){
+                cur = q.pop();
+            }else{
+                cur = q.shift();
+            }
+            var x = cur[0];
+            var y = cur[1];
+
+            seen[x][y] = true;
+
+
+            var directions = [];
+
+            directions[Phaser.LEFT] = map.getTileLeft(index, x, y);
+            directions[Phaser.RIGHT] = map.getTileRight(index, x, y);
+            directions[Phaser.UP] = map.getTileAbove(index, x, y);
+            directions[Phaser.DOWN] = map.getTileBelow(index, x, y);
+
+            // TODO
+            // make sure we don't collide with other ghosts
+
+            if(directions[Phaser.LEFT] != null && !(directions[Phaser.LEFT].index in IMPASSABLE_TERRAIN)){
+                if(x - 1 == g_x && y == g_y){
+                    this.moveGhost(Phaser.RIGHT);
+                    break;
+                }
+                if(! seen[x-1][y]){
+                    seen[x-1][y] = true;
+                    q.push([x - 1, y]);
+                }
+            }
+
+            if(directions[Phaser.RIGHT] != null && !(directions[Phaser.RIGHT].index in IMPASSABLE_TERRAIN)){
+                if(x + 1 == g_x && y == g_y){
+                    this.moveGhost(Phaser.LEFT);
+                    break;
+                }
+                if(! seen[x+1][y]){
+                    seen[x+1][y] = true;
+                    q.push([x+1, y]);
+                }
+            }
+
+            if(directions[Phaser.UP] != null && !(directions[Phaser.UP].index in IMPASSABLE_TERRAIN)) {
+                if(x == g_x && y - 1 == g_y){
+                    this.moveGhost(Phaser.DOWN);
+                    break;
+                }
+                if(! seen[x][y - 1]){
+                    seen[x][y - 1] = true;
+                    q.push([x, y - 1]);
+                }
+            }
+            if(directions[Phaser.DOWN] != null && !(directions[Phaser.DOWN].index in IMPASSABLE_TERRAIN)) {
+                if(x == g_x && y + 1 == g_y){
+                    this.moveGhost(Phaser.UP);
+                    break;
+                }
+                if(! seen[x][y + 1]){
+                    seen[x][y + 1] = true;
+                    q.push([x, y + 1]);
+                }
+            }
+        }
+    },
+
+    // TODO
+    // Improve movement of ghost be to be smooth like player.
+    moveGhost: function(direction) {
+        var local_speed = speed;
+
+        if(direction == Phaser.LEFT || direction == Phaser.UP) {
+            local_speed = -local_speed;
+        }
+
+        if(direction == Phaser.LEFT || direction == Phaser.RIGHT) {
+            ghost.body.velocity.x = local_speed;
+            ghost.body.velocity.y = 0;
+        }
+        else{
+            ghost.body.velocity.y = local_speed;
+            ghost.body.velocity.x = 0;
+        }
+    }
 };
